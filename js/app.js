fe5388d223940c2739afbb4f8e38cf82e7e6d6b0
iamamh <aungmyohtet.mlr@gmail.com>
2020-09-12 15:23:29 +0630

    before adding map

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// The jQuery XMLHttpRequest (jqXHR) object returned by $.ajax()
+
+$(document).ready(function () {
+    // $.getJSON("https://geo.ipify.org/api/v1?apiKey=at_lBAirkQljWY3y5opF9of4Sje1Pmh3",
+    //     function (data, textStatus, jqXHR) {
+    //         $('.ip-address p').text(`${data.ip}`);
+    //         $('.location p').text(`${data.location.region}, ${data.location.country}`);
+    //         $('.timezone p').text(`UTC${data.location.timezone}`);
+    //         $('.isp p').text(`${data.isp}`);
+    //     }
+    // );
+});
+
